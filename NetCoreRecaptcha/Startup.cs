a4b8d4aa3068de9777d53afc0988442bd40ed42e
Fernando Flauzino da Silva <f.flauzino@guide.com.br>
2020-04-06 14:11:14 -0300

    teste pipeline

--- NetCoreRecaptcha/Startup.cs
+++ NetCoreRecaptcha/Startup.cs
@@ -14,6 +14,8 @@ namespace NetCoreRecaptcha
             Configuration = configuration;
         }
 
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
